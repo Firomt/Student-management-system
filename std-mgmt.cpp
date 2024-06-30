5e2294480a4d2602bc0b2296861ef312ae615e3b
Firomt <firomsamt@gmail.com>
2024-06-30 16:53:46 +0300

    add storeStudent function

--- std-mgmt.cpp
+++ std-mgmt.cpp
@@ -9,3 +9,28 @@ struct Student{
     Student* prev;
     Student* next;
 };
+Student* storestudentrecord(Student*head,string name,int ID,string grade){
+    Student * newstudent= new Student();
+    newstudent->name=name;
+    newstudent->ID=ID;
+    newstudent->grade=grade;
+    newstudent->prev=NULL;
+    newstudent->next=NULL;
+    if (head==NULL)
+    {
+        head=newstudent;
+    }else{
+         Student* temp1 = head;
+      /*  while (temp1->next != NULL) {
+            temp1 = temp1->next;
+        }
+        temp1->next = newstudent;
+        newstudent->prev = temp1;
+    }*/
+   temp1->prev=newstudent;
+   newstudent->next=temp1;
+   head=newstudent;
+    
+
+    }
+    return head;}
